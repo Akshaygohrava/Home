08bdf837e46c3f975705eb12e10ffd4e131ab7f8
Akshay Gohrava <akshaygohrava@gmail.com>
2026-01-04 19:06:42 +0530

    An Authentication Added !

---
+++ JS-Files/auth.js
@@ -0,0 +1,14 @@
+const password = prompt("Enter Password to access portfolio:");
+const correctPassword = "admin@1230"; 
+
+if (password === null) {
+    
+    alert("Access denied.");
+    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+} else if (password === correctPassword) {
+    alert("Welcome to my portfolio!");
+    // window.location.href = "index.html";
+} else {
+    alert("Incorrect password. Redirecting...");
+    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+}
